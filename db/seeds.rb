dd975b035bce9574b16115cd8237345c63de51d7
nudroban <nudrob557@gmail.com>
2022-09-27 15:37:45 +0300

    add product model and view

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Product.create!(title: 'Some product', description: 
+    'Ruby is the fastest growing and most exciting dynamic language
+    out there. If you need to get working programs delivered fast,
+    you should add Ruby to your toolbox.',
+    image_url: 'image.png',
+    price: 51.05)
